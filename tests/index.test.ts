a6b12ef79edff094d1bb625c90ddf7dbbf197d5b
Jonathan Ferreira <j0n4t@hotmail.com>
2022-02-21 11:26:09 -0300

    fix: corrige caminho da lib nos testes

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
 import sinon from 'sinon';
 import { expect } from 'chai';
-import { replaceHugoWithMarkdown } from '..';
+import { replaceHugoWithMarkdown } from '../convertHugoToMarkdown';
 
 describe('transforma abertura para markdown', () => {
   it('Verifica tag {{< highlight react >}}', () => {
